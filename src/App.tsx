b97310ff6518a3e460ca78d326f23ddc88f8dbe9
Luis Fernández <dev.oreku@gmail.com>
2022-05-04 13:24:59 -0500

    lorem ipsum dolor

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <div className="App">
+      <p>Lorem ipsum dolor sit.</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
